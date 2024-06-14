c587005374ac1d4336190bbe27146030e43a7290
Dickless <37750743+Dicklessgreat@users.noreply.github.com>
2024-06-14 14:52:34 +0900

    ほかのピンも追加した

--- src/pins.rs
+++ src/pins.rs
@@ -35,3 +35,65 @@ pub type SeedPin29 = hal::peripherals::PB14; // PIN_36, USB1 D-, USART1 Tx
 pub type SeedPin30 = hal::peripherals::PB15; // PIN_37, USB1 D+, USART1 Rx
 
 pub type LedUserPin = hal::peripherals::PC7; // LED_USER
+
+#[allow(non_snake_case)]
+pub struct USB2Pins {
+    pub DN: hal::peripherals::PA11, // USB2 D-
+    pub DP: hal::peripherals::PA12, // USB2 D+
+}
+
+#[allow(non_snake_case)]
+pub struct FMCPins {
+    // https://github.com/electro-smith/libDaisy/blob/3dda55e9ed55a2f8b6bc4fa6aa2c7ae134c317ab/src/per/qspi.c#L695
+    pub IO0: hal::peripherals::PF8, // (SI)
+    pub IO1: hal::peripherals::PF9, // (SO)
+    pub IO2: hal::peripherals::PF7,
+    pub IO3: hal::peripherals::PF6,
+    pub SCK: hal::peripherals::PF10,
+    pub CS: hal::peripherals::PG6,
+}
+
+// - Pins ---------------------------------------------------------------------
+
+#[allow(non_snake_case)]
+pub struct Pins {
+    // https://github.com/electro-smith/DaisyWiki/wiki/2.-Daisy-Seed-Pinout
+    pub SEED_PIN_0: SeedPin0,
+    pub SEED_PIN_1: SeedPin1,
+    pub SEED_PIN_2: SeedPin2,
+    pub SEED_PIN_3: SeedPin3,
+    pub SEED_PIN_4: SeedPin4,
+    pub SEED_PIN_5: SeedPin5,
+    pub SEED_PIN_6: SeedPin6,
+    pub SEED_PIN_7: SeedPin7,
+    pub SEED_PIN_8: SeedPin8,
+    pub SEED_PIN_9: SeedPin9,
+    pub SEED_PIN_10: SeedPin10,
+    pub SEED_PIN_11: SeedPin11,
+    pub SEED_PIN_12: SeedPin12,
+    pub SEED_PIN_13: SeedPin13,
+    pub SEED_PIN_14: SeedPin14,
+    pub SEED_PIN_15: SeedPin15,
+    pub SEED_PIN_16: SeedPin16,
+    pub SEED_PIN_17: SeedPin17,
+    pub SEED_PIN_18: SeedPin18,
+    pub SEED_PIN_19: SeedPin19,
+    pub SEED_PIN_20: SeedPin20,
+    pub SEED_PIN_21: SeedPin21,
+    pub SEED_PIN_22: SeedPin22,
+    pub SEED_PIN_23: SeedPin23,
+    pub SEED_PIN_24: SeedPin24,
+    pub SEED_PIN_25: SeedPin25,
+    pub SEED_PIN_26: SeedPin26,
+    pub SEED_PIN_27: SeedPin27,
+    pub SEED_PIN_28: SeedPin28,
+    pub SEED_PIN_29: SeedPin29,
+    pub SEED_PIN_30: SeedPin30,
+
+    // board peripherals
+    pub LED_USER: LedUserPin,
+    // pub AK4556: AK4556Pins,
+    pub FMC: FMCPins,
+    pub SDRAM: (), // TODO
+    pub USB2: USB2Pins,
+}
