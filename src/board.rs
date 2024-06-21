f2ef265037d10249bf4c148f0964fbcd52608e74
Dickless <37750743+Dicklessgreat@users.noreply.github.com>
2024-06-22 01:26:24 +0900

    DaisyBoard::new()を追加した

--- src/board.rs
+++ src/board.rs
@@ -44,3 +44,65 @@ pub struct DaisyBoard<'a> {
     pub SDRAM: (), // TODO
     pub USB2: USB2Pins,
 }
+
+impl<'a> DaisyBoard<'a> {
+    pub fn new(config: embassy_stm32::Config) -> Self {
+        let p = embassy_stm32::init(config);
+        Self {
+            SEED_PIN_0: p.PB12,
+            SEED_PIN_1: p.PC11,
+            SEED_PIN_2: p.PC10,
+            SEED_PIN_3: p.PC9,
+            SEED_PIN_4: p.PC8,
+            SEED_PIN_5: p.PD2,
+            SEED_PIN_6: p.PC12,
+            SEED_PIN_7: p.PG10,
+            SEED_PIN_8: p.PG11,
+            SEED_PIN_9: p.PB4,
+            SEED_PIN_10: p.PB5,
+            SEED_PIN_11: p.PB8,
+            SEED_PIN_12: p.PB9,
+            SEED_PIN_13: p.PB6,
+            SEED_PIN_14: p.PB7,
+            SEED_PIN_15: p.PC0,
+            SEED_PIN_16: p.PA3,
+            SEED_PIN_17: p.PB1,
+            SEED_PIN_18: p.PA7,
+            SEED_PIN_19: p.PA6,
+            SEED_PIN_20: p.PC1,
+            SEED_PIN_21: p.PC4,
+            SEED_PIN_22: p.PA5,
+            SEED_PIN_23: p.PA4,
+            SEED_PIN_24: p.PA1,
+            SEED_PIN_25: p.PA0,
+            SEED_PIN_26: p.PD11,
+            SEED_PIN_27: p.PG9,
+            SEED_PIN_28: p.PA2,
+            SEED_PIN_29: p.PB14,
+            SEED_PIN_30: p.PB15,
+            LED_USER: UserLed::new(p.PC7),
+            WM8731: WM8731Pins {
+                SCL: p.PH4,
+                SDA: p.PB11,
+                MCLK_A: p.PE2,
+                SCK_A: p.PE5,
+                FS_A: p.PE4,
+                SD_A: p.PE6,
+                SD_B: p.PE3,
+            },
+            FMC: FMCPins {
+                IO0: p.PF8,
+                IO1: p.PF9,
+                IO2: p.PF7,
+                IO3: p.PF6,
+                SCK: p.PF10,
+                CS: p.PG6,
+            },
+            SDRAM: (),
+            USB2: USB2Pins {
+                DN: p.PA11, // USB2 D-
+                DP: p.PA12, // USB2 D+
+            },
+        }
+    }
+}
