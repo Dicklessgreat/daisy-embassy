7ad378c92484c07a50e200d6a9998be20bb38f1b
Dickless <011001nobuo@gmail.com>
2024-06-26 15:53:42 +0900

    add macro to init DaisyPeripherals

--- examples/passthrough/src/main.rs
+++ examples/passthrough/src/main.rs
@@ -3,9 +3,9 @@
 
 use daisy_embassy::{
     audio::{Fs, InterleavedBlock, Start, HALF_DMA_BUFFER_LENGTH},
-    board::DaisyPeripherals,
     embassy_sync::{blocking_mutex::raw::NoopRawMutex, zerocopy_channel::Channel},
     hal::{self, time::Hertz},
+    new_daisy_p,
     pins::{DaisyPins, USB2Pins, WM8731Pins},
     DaisyBoard,
 };
@@ -48,62 +48,7 @@ async fn main(_spawner: Spawner) {
     }
 
     let p = hal::init(config);
-    let daisy_p = DaisyPeripherals {
-        daisy_pins: DaisyPins {
-            SEED_PIN_0: p.PB12,
-            SEED_PIN_1: p.PC11,
-            SEED_PIN_2: p.PC10,
-            SEED_PIN_3: p.PC9,
-            SEED_PIN_4: p.PC8,
-            SEED_PIN_5: p.PD2,
-            SEED_PIN_6: p.PC12,
-            SEED_PIN_7: p.PG10,
-            SEED_PIN_8: p.PG11,
-            SEED_PIN_9: p.PB4,
-            SEED_PIN_10: p.PB5,
-            SEED_PIN_11: p.PB8,
-            SEED_PIN_12: p.PB9,
-            SEED_PIN_13: p.PB6,
-            SEED_PIN_14: p.PB7,
-            SEED_PIN_15: p.PC0,
-            SEED_PIN_16: p.PA3,
-            SEED_PIN_17: p.PB1,
-            SEED_PIN_18: p.PA7,
-            SEED_PIN_19: p.PA6,
-            SEED_PIN_20: p.PC1,
-            SEED_PIN_21: p.PC4,
-            SEED_PIN_22: p.PA5,
-            SEED_PIN_23: p.PA4,
-            SEED_PIN_24: p.PA1,
-            SEED_PIN_25: p.PA0,
-            SEED_PIN_26: p.PD11,
-            SEED_PIN_27: p.PG9,
-            SEED_PIN_28: p.PA2,
-            SEED_PIN_29: p.PB14,
-            SEED_PIN_30: p.PB15,
-        },
-        led_user_pin: p.PC7,
-        wm8731_pin: WM8731Pins {
-            SCL: p.PH4,
-            SDA: p.PB11,
-            MCLK_A: p.PE2,
-            SCK_A: p.PE5,
-            FS_A: p.PE4,
-            SD_A: p.PE6,
-            SD_B: p.PE3,
-        },
-        audio_peripherals: daisy_embassy::audio::Peripherals {
-            sai1: p.SAI1,
-            i2c2: p.I2C2,
-            dma1_ch1: p.DMA1_CH1,
-            dma1_ch2: p.DMA1_CH2,
-        },
-        usb2_pins: USB2Pins {
-            DN: p.PA11,
-            DP: p.PA12,
-        },
-        usb_otg_fs: p.USB_OTG_FS,
-    };
+    let daisy_p = new_daisy_p!(p);
     let board = DaisyBoard::new(daisy_p, Fs::Fs48000, Fs::Fs48000);
     let mut interface = board.interface;
 
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,65 @@ pub use audio::Fs;
 pub use board::DaisyBoard;
 pub use embassy_stm32 as hal;
 pub use embassy_sync;
+
+#[macro_export]
+macro_rules! new_daisy_p {
+    ($p:ident) => {
+        daisy_embassy::board::DaisyPeripherals {
+            daisy_pins: DaisyPins {
+                SEED_PIN_0: $p.PB12,
+                SEED_PIN_1: $p.PC11,
+                SEED_PIN_2: $p.PC10,
+                SEED_PIN_3: $p.PC9,
+                SEED_PIN_4: $p.PC8,
+                SEED_PIN_5: $p.PD2,
+                SEED_PIN_6: $p.PC12,
+                SEED_PIN_7: $p.PG10,
+                SEED_PIN_8: $p.PG11,
+                SEED_PIN_9: $p.PB4,
+                SEED_PIN_10: $p.PB5,
+                SEED_PIN_11: $p.PB8,
+                SEED_PIN_12: $p.PB9,
+                SEED_PIN_13: $p.PB6,
+                SEED_PIN_14: $p.PB7,
+                SEED_PIN_15: $p.PC0,
+                SEED_PIN_16: $p.PA3,
+                SEED_PIN_17: $p.PB1,
+                SEED_PIN_18: $p.PA7,
+                SEED_PIN_19: $p.PA6,
+                SEED_PIN_20: $p.PC1,
+                SEED_PIN_21: $p.PC4,
+                SEED_PIN_22: $p.PA5,
+                SEED_PIN_23: $p.PA4,
+                SEED_PIN_24: $p.PA1,
+                SEED_PIN_25: $p.PA0,
+                SEED_PIN_26: $p.PD11,
+                SEED_PIN_27: $p.PG9,
+                SEED_PIN_28: $p.PA2,
+                SEED_PIN_29: $p.PB14,
+                SEED_PIN_30: $p.PB15,
+            },
+            led_user_pin: $p.PC7,
+            wm8731_pin: WM8731Pins {
+                SCL: $p.PH4,
+                SDA: $p.PB11,
+                MCLK_A: $p.PE2,
+                SCK_A: $p.PE5,
+                FS_A: $p.PE4,
+                SD_A: $p.PE6,
+                SD_B: $p.PE3,
+            },
+            audio_peripherals: daisy_embassy::audio::Peripherals {
+                sai1: $p.SAI1,
+                i2c2: $p.I2C2,
+                dma1_ch1: $p.DMA1_CH1,
+                dma1_ch2: $p.DMA1_CH2,
+            },
+            usb2_pins: USB2Pins {
+                DN: $p.PA11,
+                DP: $p.PA12,
+            },
+            usb_otg_fs: $p.USB_OTG_FS,
+        }
+    };
+}
