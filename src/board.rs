f301de7f737a209635a9edf5bc135f7e3c16af88
Dickless <011001nobuo@gmail.com>
2024-07-03 02:40:43 +0900

    change usb initialization.

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,7 @@
 use crate::audio::{self, AudioPeripherals};
+use crate::led::UserLed;
 use crate::pins::*;
-use crate::{led::UserLed, usb::DaisyUsb};
+use crate::usb::UsbPeripherals;
 use embassy_stm32 as hal;
 use hal::peripherals::USB_OTG_FS;
 use hal::{bind_interrupts, i2c, peripherals, usb};
@@ -19,7 +20,7 @@ pub struct DaisyBoard<'a> {
     pub audio_peripherals: AudioPeripherals,
     pub FMC: (),   //TODO
     pub SDRAM: (), // TODO
-    pub daisy_usb: DaisyUsb,
+    pub usb_peripherals: UsbPeripherals,
     // on board "BOOT" button.
     pub boot: Boot,
 }
@@ -35,14 +36,16 @@ pub struct DaisyPeripherals {
 
 impl<'a> DaisyBoard<'a> {
     pub fn new(p: DaisyPeripherals) -> Self {
-        let usb_driver = crate::usb::init(p.usb_otg_fs, p.usb2_pins);
         Self {
             pins: p.daisy_pins,
             user_led: UserLed::new(p.led_user_pin),
             audio_peripherals: p.audio_peripherals,
             FMC: (),
             SDRAM: (),
-            daisy_usb: usb_driver,
+            usb_peripherals: UsbPeripherals {
+                pins: p.usb2_pins,
+                usb_otg_fs: p.usb_otg_fs,
+            },
             boot: p.boot,
         }
     }
--- src/usb.rs
+++ src/usb.rs
@@ -9,14 +9,28 @@ use crate::{board::Irqs, pins::USB2Pins};
 
 pub type DaisyUsb = Driver<'static, USB_OTG_FS>;
 
-pub fn init(usb_otg_fs: USB_OTG_FS, pins: USB2Pins) -> DaisyUsb {
-    let mut config = Config::default();
-    // Do not enable vbus_detection. This is a safe default that works in all boards.
-    // However, if your USB device is self-powered (can stay powered on if USB is unplugged), you need
-    // to enable vbus_detection to comply with the USB spec. If you enable it, the board
-    // has to support it or USB won't work at all. See docs on `vbus_detection` for details.
-    config.vbus_detection = false;
-    static EP_OUT_BUFFER: StaticCell<[u8; 256]> = StaticCell::new();
-    let ep_out_buffer = EP_OUT_BUFFER.init([0; 256]);
-    Driver::new_fs(usb_otg_fs, Irqs, pins.DP, pins.DN, ep_out_buffer, config)
+pub struct UsbPeripherals {
+    pub usb_otg_fs: USB_OTG_FS,
+    pub pins: USB2Pins,
+}
+
+impl UsbPeripherals {
+    pub fn build(self) -> DaisyUsb {
+        let mut config = Config::default();
+        // Do not enable vbus_detection. This is a safe default that works in all boards.
+        // However, if your USB device is self-powered (can stay powered on if USB is unplugged), you need
+        // to enable vbus_detection to comply with the USB spec. If you enable it, the board
+        // has to support it or USB won't work at all. See docs on `vbus_detection` for details.
+        config.vbus_detection = false;
+        static EP_OUT_BUFFER: StaticCell<[u8; 256]> = StaticCell::new();
+        let ep_out_buffer = EP_OUT_BUFFER.init([0; 256]);
+        Driver::new_fs(
+            self.usb_otg_fs,
+            Irqs,
+            self.pins.DP,
+            self.pins.DN,
+            ep_out_buffer,
+            config,
+        )
+    }
 }
