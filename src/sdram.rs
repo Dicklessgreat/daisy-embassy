e40a49b30a757807522a7be2e1f109093581407c
Dickless <011001nobuo@gmail.com>
2024-07-07 20:57:06 +0900

    add cortex settings of FMC(only copy and paste from embassy/example)

--- src/sdram.rs
+++ src/sdram.rs
@@ -1,4 +1,5 @@
 use crate::pins::SdRamPins;
+use cortex_m::peripheral::{MPU, SCB};
 use embassy_stm32 as hal;
 use hal::fmc::Fmc;
 use hal::peripherals::FMC;
@@ -11,7 +12,81 @@ pub struct SdRamBuilder {
 }
 
 impl SdRamBuilder {
-    pub fn build<'a>(self) -> Sdram<Fmc<'a, FMC>, Is42s32160ge75bli> {
+    pub fn build<'a>(self, mpu: &mut MPU, scb: &mut SCB) -> Sdram<Fmc<'a, FMC>, Is42s32160ge75bli> {
+        // Configure MPU for external SDRAM
+        // MPU config for SDRAM write-through
+        let sdram_size = 64 * 1024 * 1024;
+        // Refer to ARM®v7-M Architecture Reference Manual ARM DDI 0403
+        // Version E.b Section B3.5
+        const MEMFAULTENA: u32 = 1 << 16;
+        unsafe {
+            /* Make sure outstanding transfers are done */
+            cortex_m::asm::dmb();
+
+            scb.shcsr.modify(|r| r & !MEMFAULTENA);
+
+            /* Disable the MPU and clear the control register*/
+            mpu.ctrl.write(0);
+        }
+
+        const REGION_NUMBER0: u32 = 0x00;
+        const REGION_BASE_ADDRESS: u32 = 0xD000_0000;
+
+        const REGION_FULL_ACCESS: u32 = 0x03;
+        const REGION_CACHEABLE: u32 = 0x01;
+        const REGION_WRITE_BACK: u32 = 0x01;
+        const REGION_ENABLE: u32 = 0x01;
+
+        assert_eq!(
+            sdram_size & (sdram_size - 1),
+            0,
+            "SDRAM memory region size must be a power of 2"
+        );
+        assert_eq!(
+            sdram_size & 0x1F,
+            0,
+            "SDRAM memory region size must be 32 bytes or more"
+        );
+        fn log2minus1(sz: u32) -> u32 {
+            for i in 5..=31 {
+                if sz == (1 << i) {
+                    return i - 1;
+                }
+            }
+            panic!("Unknown SDRAM memory region size!");
+        }
+
+        // Configure region 0
+        //
+        // Cacheable, outer and inner write-back, no write allocate. So
+        // reads are cached, but writes always write all the way to SDRAM
+        unsafe {
+            mpu.rnr.write(REGION_NUMBER0);
+            mpu.rbar.write(REGION_BASE_ADDRESS);
+            mpu.rasr.write(
+                (REGION_FULL_ACCESS << 24)
+                    | (REGION_CACHEABLE << 17)
+                    | (REGION_WRITE_BACK << 16)
+                    | (log2minus1(sdram_size as u32) << 1)
+                    | REGION_ENABLE,
+            );
+        }
+
+        const MPU_ENABLE: u32 = 0x01;
+        const MPU_DEFAULT_MMAP_FOR_PRIVILEGED: u32 = 0x04;
+
+        // Enable
+        unsafe {
+            mpu.ctrl
+                .modify(|r| r | MPU_DEFAULT_MMAP_FOR_PRIVILEGED | MPU_ENABLE);
+
+            scb.shcsr.modify(|r| r | MEMFAULTENA);
+
+            // Ensure MPU settings take effect
+            cortex_m::asm::dsb();
+            cortex_m::asm::isb();
+        }
+
         let Self { pins, instance } = self;
         Fmc::sdram_a12bits_d32bits_4banks_bank1(
             instance,
