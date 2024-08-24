34b0a19549d5e1339034337046153a62045eb905
Dickless <011001nobuo@gmail.com>
2024-08-25 04:37:53 +0900

    temporal fix

--- src/usb.rs
+++ src/usb.rs
@@ -5,7 +5,8 @@ use hal::{
 };
 use static_cell::StaticCell;
 
-use crate::{board::Irqs, pins::USB2Pins};
+// use crate::{board::Irqs, pins::USB2Pins};
+use crate::pins::USB2Pins;
 
 pub type DaisyUsb = Driver<'static, USB_OTG_FS>;
 
@@ -18,13 +19,14 @@ impl UsbPeripherals {
     pub fn build(self, config: Config) -> DaisyUsb {
         static EP_OUT_BUFFER: StaticCell<[u8; 256]> = StaticCell::new();
         let ep_out_buffer = EP_OUT_BUFFER.init([0; 256]);
-        Driver::new_fs(
-            self.usb_otg_fs,
-            Irqs,
-            self.pins.DP,
-            self.pins.DN,
-            ep_out_buffer,
-            config,
-        )
+        todo!()
+        // Driver::new_fs(
+        //     self.usb_otg_fs,
+        //     Irqs,
+        //     self.pins.DP,
+        //     self.pins.DN,
+        //     ep_out_buffer,
+        //     config,
+        // )
     }
 }
