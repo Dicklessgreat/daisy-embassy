d73cc7cca06d6ded09f61ae684399d0347d13fee
Niclas Wesemann <niclaswesemann@gmail.com>
2024-09-06 12:40:08 +0200

    blink while audio passthrough to show sign of life
    Signed-off-by: Niclas Wesemann <niclaswesemann@gmail.com>

--- examples/passthrough.rs
+++ examples/passthrough.rs
@@ -1,17 +1,41 @@
+// Audio passthrough example for daisy seed
+// Currently support for WM8731 codec and PCM3060 codec
+// For WM8731 use feature "seed_1_1"
+// For PCM3060 use feature "seed_1_2"
+//
+// Just like they did in https://github.com/zlosynth/daisy
 #![no_std]
 #![no_main]
 
-use daisy_embassy::{hal, new_daisy_board, audio::HALF_DMA_BUFFER_LENGTH};
-use defmt::debug;
+use embassy_time::Timer;
+use daisy_embassy::{audio::HALF_DMA_BUFFER_LENGTH, hal, led::{self, UserLed}, new_daisy_board, DaisyBoard};
+use defmt::{debug, info};
 use embassy_executor::Spawner;
 use {defmt_rtt as _, panic_probe as _};
 
+
+#[embassy_executor::task]
+async fn blink(mut led: UserLed<'static>) {
+    // Blink LED while audio passthrough to show sign of life
+    loop {
+        led.on();
+        Timer::after_millis(500).await;
+
+        led.off();
+        Timer::after_millis(500).await;
+    }
+}
+
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     debug!("====program start====");
     let config = daisy_embassy::default_rcc();
     let p = hal::init(config);
-    let board = new_daisy_board!(p);
+    let board: DaisyBoard<'_> = new_daisy_board!(p);
+    
+    let led = board.user_led;
+    spawner.spawn(blink(led)).unwrap();
+    
     let mut interface = board
         .audio_peripherals
         .prepare_interface(Default::default())
