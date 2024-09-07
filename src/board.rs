87c6b0e5104b9abe15ca181d13f576dbd203bdae
Dickless <011001nobuo@gmail.com>
2024-09-08 02:18:55 +0900

    suppress warnings

--- src/board.rs
+++ src/board.rs
@@ -3,7 +3,6 @@ use crate::led::UserLed;
 use crate::pins::*;
 use crate::usb::UsbPeripherals;
 use crate::{audio::AudioPeripherals, sdram::SdRamBuilder};
-use embassy_stm32 as hal;
 pub struct DaisyBoard<'a> {
     pub pins: DaisyPins,
     pub user_led: UserLed<'a>,
